3afb9ae3b8c869b40c85e95d694e1cd4b83a0057
axel <axel.salmeron@globallogic.com>
2024-06-06 16:49:46 -0500

    feat: link to input signal example in default route card

--- projects/demo/src/app/content-projection-container/content-projection-container.component.ts
+++ projects/demo/src/app/content-projection-container/content-projection-container.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { WidgetComponent } from '@shared/lib/widget/widget.component';
 
 @Component({
   selector: 'app-content-projection-container',
   standalone: true,
-  imports: [WidgetComponent],
+  imports: [WidgetComponent, RouterLink],
   templateUrl: './content-projection-container.component.html',
   styleUrl: './content-projection-container.component.scss'
 })
