3a750f8c0eee34f6fe07c180135b0412c7220e02
axel <axel.salmeron@globallogic.com>
2024-06-24 11:09:23 -0500

    feat(kendogridcomponent): adds export to csv basic functionality

--- projects/shared/src/lib/kendo-grid/kendo-grid.component.ts
+++ projects/shared/src/lib/kendo-grid/kendo-grid.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DataStateChangeEvent, GridDataResult, GridModule, } from '@progress/kendo-angular-grid';
 import { State } from '@progress/kendo-data-query';
+import { encodeBase64, saveAs } from '@progress/kendo-file-saver';
+import { json2csv } from 'json-2-csv';
 import { DataService } from '../data.service';
 
 @Component({
@@ -34,6 +36,16 @@ export class KendoGridComponent {
     this.loading = true;
     this.dataService.fetch(state).subscribe((response: GridDataResult) => {
       this.data = response;
+      this.loading = false;
+    });
+  }
+
+  public exportToCSV() {
+    this.loading = true;
+    this.dataService.fetch({}).subscribe((response: GridDataResult) => {
+      const csv = json2csv(response.data)
+      const dataURI = 'data:text/plain;base64,' + encodeBase64(csv);
+      saveAs(dataURI, 'gridData.csv');
       this.loading = false;
     });
   }
