94ec876ca88a3da76568ac3b180105781d7ef6c5
axel <axel.salmeron@globallogic.com>
2024-07-04 14:33:42 -0500

    feat(kendogridcomponent): adds hidden columns support

--- projects/shared/src/lib/kendo-grid/kendo-grid.component.ts
+++ projects/shared/src/lib/kendo-grid/kendo-grid.component.ts
@@ -1,14 +1,15 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { DataStateChangeEvent, GridDataResult, GridModule, } from '@progress/kendo-angular-grid';
 import { State } from '@progress/kendo-data-query';
-import { encodeBase64, saveAs } from '@progress/kendo-file-saver';
-import { json2csv } from 'json-2-csv';
+// import { encodeBase64, saveAs } from '@progress/kendo-file-saver';
+// import { json2csv } from 'json-2-csv';
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'lib-kendo-grid',
   standalone: true,
-  imports: [GridModule],
+  imports: [GridModule, JsonPipe],
   templateUrl: './kendo-grid.component.html',
   styleUrl: './kendo-grid.component.scss'
 })
@@ -22,6 +23,8 @@ export class KendoGridComponent {
   };
   public loading = false;
   public data: GridDataResult = { data: [], total: 0 };
+  public columns: string[] = ['customerID', 'contactName', 'address', 'companyName', 'city'];
+  public hiddenColumns: string[] = ['addres', 'companyName', 'city'];
 
   constructor(private dataService: DataService) {
     this.sendRequest(this.state);
@@ -42,11 +45,12 @@ export class KendoGridComponent {
 
   public exportToCSV() {
     this.loading = true;
-    this.dataService.fetch({}).subscribe((response: GridDataResult) => {
-      const csv = json2csv(response.data)
-      const dataURI = 'data:text/plain;base64,' + encodeBase64(csv);
-      saveAs(dataURI, 'gridData.csv');
-      this.loading = false;
+    this.dataService.fetch({ ...this.state, take: undefined, skip: undefined }).subscribe((response: GridDataResult) => {
+      console.log(response.data);
+      // const csv = json2csv(response.data)
+      // const dataURI = 'data:text/plain;base64,' + encodeBase64(csv);
+      // saveAs(dataURI, 'gridData.csv');
+      // this.loading = false;
     });
   }
 }
